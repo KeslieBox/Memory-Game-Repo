ad9ec40d4f6ceffb89a11eafabf768ffb3310640
KeslieBox <kesliebox@gmail.com>
2021-08-04 15:31:34 -0500

    card flip logic working, need to set up shuffle

--- labyrinth_memory_game/src/MainCanvas.js
+++ labyrinth_memory_game/src/MainCanvas.js
@@ -17,64 +17,64 @@ function MainCanvas(props) {
   return (
     <section className="main">
       {/* want to add a background image of a maze or something */}
-      <div className="memory-card" data-framework='reactbadge'>
+      <div className="memory-card" dataset='reactbadge'>
         <img onClick={e => props.clickCard(e)} className="front-face" src={reactbadge} alt="React"/>
-        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="React"/>
       </div>
     
-      <div className="memory-card" data-framework='reactbadge'>
-        <img className="front-face" src={reactbadge} alt="React"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
-      </div>
-    
-      <div className="memory-card" data-framework='angular'>
-        <img className="front-face" src={angular} alt="Angular"/>
-        <img class="back-face" src={backface} alt="Memory Card"/>
+      <div className="memory-card" dataset='reactbadge'>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={reactbadge} alt="React"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="React"/>
       </div>
     
       <div className="memory-card" data-framework='angular'>
-        <img className="front-face" src={angular} alt="Angular"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={angular} alt="Angular"/>
+        <img onClick={e => props.clickCard(e)} class="back-face" src={backface} alt="Angular"/>
+      </div>
+    
+      <div className="memory-card" data-framework='angular'>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={angular} alt="Angular"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Angular"/>
       </div>
     
       <div className="memory-card" data-framework='ember'>
-        <img className="front-face" src={ember} alt="Ember"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={ember} alt="Ember"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Ember"/>
       </div>
     
       <div className="memory-card" data-framework='ember'>
-        <img className="front-face" src={ember} alt="Ember"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={ember} alt="Ember"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Ember"/>
       </div>
     
       <div className="memory-card" data-framework='vue'>
-        <img className="front-face" src={vue} alt="Vue"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={vue} alt="Vue"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Vue"/>
       </div>
     
       <div className="memory-card" data-framework='vue'>
-        <img className="front-face" src={vue} alt="Vue"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={vue} alt="Vue"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Vue"/>
       </div>
 
       <div className="memory-card" data-framework='backbone'>
-        <img className="front-face" src={backbone} alt="Backbone"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={backbone} alt="Backbone"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Backbone"/>
       </div>
 
       <div className="memory-card" data-framework='backbone'>
-        <img className="front-face" src={backbone} alt="Backbone"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={backbone} alt="Backbone"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Backbone"/>
       </div>
 
       <div className="memory-card" data-framework='aurelia'>
-        <img className="front-face" src={aurelia} alt="Aurelia"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={aurelia} alt="Aurelia"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Aurelia"/>
       </div>
 
       <div className="memory-card" data-framework='aurelia'>
-        <img className="front-face" src={aurelia} alt="Aurelia"/>
-        <img className="back-face" src={backface} alt="Memory Card"/>
+        <img onClick={e => props.clickCard(e)} className="front-face" src={aurelia} alt="Aurelia"/>
+        <img onClick={e => props.clickCard(e)} className="back-face" src={backface} alt="Aurelia"/>
       </div>
 
     </section>
--- labyrinth_memory_game/src/MemoryLogic.js
+++ labyrinth_memory_game/src/MemoryLogic.js
@@ -1,30 +1,85 @@
 import MainCanvas from './MainCanvas'
 
-function MemoryLogic(props) {
+function MemoryLogic(props){
     // how to access cards from virtual DOM?
     const cards = document.querySelectorAll('.memory-card')
     let hasFlippedCard = false
     let lockBoard = false
     let firstCard, secondCard
 
-    debugger
-
     // if (lockBoard) return
     // if (this === firstCard) return
 
-    const handleClickCard = (e) => {
-        // handle clikc card here
+    function handleClickCard(e){
+        // handle click card here
         debugger
         if (lockBoard) return
-        if (this === firstCard) return
-        e.classList.toggle('flip')
+        // if (firstCard && e.target.className === firstCard.target.className) return
+        e.target.parentElement.classList.add('flip')
+
+        if (!hasFlippedCard) {
+            debugger
+            // first click
+            hasFlippedCard = true
+            firstCard = e
+    
+            return 
+        } 
+        // second click 
+        hasFlippedCard = false
+        secondCard = e
+
+        checkForMatch() 
     }
+
+    function checkForMatch(){
+        debugger
+        // should not be based on classname but on the name of the particular card
+        let isMatch = firstCard.target.alt === secondCard.target.alt
+        
+        isMatch ? disableCards() : unflipCards()
+    }
+
+    function disableCards(){
+        debugger
+        // if the cards matched, we remove event listener so they no longer respond to click
+        firstCard.target.removeEventListener('click', handleClickCard)
+        secondCard.target.removeEventListener('click', handleClickCard)
+    
+        resetBoard()
+    }
+    
+    function unflipCards(){
+        debugger
+        // if cards don't match, we lock the board to prevent other moves 
+        lockBoard = true
+        // we set a timer before flipping cards back over
+        setTimeout(() => {
+            debugger
+            firstCard.target.parentElement.classList.remove('flip')
+            secondCard.target.parentElement.classList.remove('flip')
+            // we unlock the board again for new moves
+            resetBoard()
+        }, 1500)
+    }
+
+    function resetBoard(){
+        [hasFlippedCard, lockBoard] = [false, false]
+        [firstCard, secondCard] = [null, null]
+    }
+    
+    // wrapped in parenthesis to immediately call the function expression right after it's defined
+    (function shuffle(){
+        // how to get all the cards
+        // cards.forEach(card => {
+        //     let randomPos = Math.floor(Math.random() * 12)
+        //     card.style.order = randomPos
+        // })
+    })()
 
     return (
         <MainCanvas clickCard={handleClickCard}/>
-    )
-    
-
+    ) 
 } 
 
 export default MemoryLogic
